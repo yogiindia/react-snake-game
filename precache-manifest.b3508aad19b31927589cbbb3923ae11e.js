aa231f0f38cc39b2fa2e885f3e1b73c8a4f578bf
yogesh kumar <yogeshkumar87@hotmail.com>
2019-05-23 23:00:19 +0530

    Updates

---
+++ precache-manifest.b3508aad19b31927589cbbb3923ae11e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef309031a508078e390ed94930570a8f",
+    "url": "/yogeshkumar87/react-snake-game/index.html"
+  },
+  {
+    "revision": "338f128cf4b4cc1d7635",
+    "url": "/yogeshkumar87/react-snake-game/static/css/main.eb8ff75e.chunk.css"
+  },
+  {
+    "revision": "89a697531ebfd9aa5571",
+    "url": "/yogeshkumar87/react-snake-game/static/js/2.2dc9da19.chunk.js"
+  },
+  {
+    "revision": "338f128cf4b4cc1d7635",
+    "url": "/yogeshkumar87/react-snake-game/static/js/main.f100f29e.chunk.js"
+  },
+  {
+    "revision": "804463ad1fee865fb8db",
+    "url": "/yogeshkumar87/react-snake-game/static/js/runtime~main.c8ab43ef.js"
+  }
+]);
